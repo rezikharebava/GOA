e60ee0cb2c8c98e735a313c24beafcecc8659327
rezikharebava <kharebavarezikoo.com>
2025-02-02 01:01:07 -0500

    43

---
+++ level 42/CW/CW42.py
@@ -0,0 +1,21 @@
+def points(game):
+    res = 0
+    for score in game:
+        if score[0] > score[2]:
+            res +=3
+        elif score[0] > score[2]:
+            res += 0
+        else:
+            res += 1
+    return res
+
+
+
+age = 14
+binary_age = ""
+
+while age > 0:
+    binary_age = str(age % 2) + binary_age
+    age = age // 2
+
+print(binary_age)
