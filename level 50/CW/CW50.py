f0c44258d9b8434fadfdeac132730d72836ae845
rezikharebava <kharebavarezikoo.com>
2025-03-15 11:21:36 -0400

    zd

--- level 50/CW/CW50.py
+++ level 50/CW/CW50.py
@@ -1,1 +1,5 @@
- "grop47 are goa groups".split(" ")
+# clsswork 1
+def is_isogram(string):
+    lower_string = string.lower()
+    char_counts = [lower_string.count(char) for char in lower_string]
+    return all(count == 1 for count in char_counts)
